c7b064c0dd22a1308a79f097131c0d76e35020d1
FiveTW87 <thanwalokwattanakul@gmail.com>
2023-12-27 14:57:45 +0700

    feat : complete ex

--- client/src/components/EditProductForm.jsx
+++ client/src/components/EditProductForm.jsx
@@ -1,6 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 function EditProductForm() {
+  const navigate = useNavigate();
+  const param = useParams();
+  const [inputName, setInputName] = useState("");
+  const [inputImg, setInputImg] = useState("");
+  const [inputPrice, setInputPrice] = useState();
+  const [inputDetail, setInputDetail] = useState("");
+  const getProduct = async () => {
+    const result = await axios.get(
+      `http://localhost:4001/products/${param.id}`
+    );
+    const product = result.data.data;
+    setInputName(product.name);
+    setInputImg(product.image);
+    setInputPrice(product.price);
+    setInputDetail(product.description);
+    console.log(result.data.data);
+  };
+  const putProduct = async () => {
+    await axios.put(`http://localhost:4001/products/${param.id}`, {
+      name: inputName,
+      image: inputImg,
+      price: inputPrice,
+      description: inputDetail,
+    });
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    putProduct();
+    navigate("/");
+  };
   return (
-    <form className="product-form">
+    <form className="product-form" onSubmit={handleOnSubmit}>
       <h1>Edit Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,7 +46,10 @@ function EditProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={(e) => {
+              setInputName(e.target.value);
+            }}
+            value={inputName}
           />
         </label>
       </div>
@@ -22,7 +61,10 @@ function EditProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={(e) => {
+              setInputImg(e.target.value);
+            }}
+            value={inputImg}
           />
         </label>
       </div>
@@ -34,7 +76,10 @@ function EditProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={(e) => {
+              setInputPrice(e.target.value);
+            }}
+            value={inputPrice}
           />
         </label>
       </div>
@@ -46,9 +91,12 @@ function EditProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={(e) => {
+              setInputDetail(e.target.value);
+            }}
             rows={4}
             cols={30}
+            value={inputDetail}
           />
         </label>
       </div>
--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,12 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 function ViewProductPage() {
+  const param = useParams();
+  const [product, setProduct] = useState({});
+  const navigate = useNavigate();
+  const getProduct = async () => {
+    const result = await axios.get(
+      `http://localhost:4001/products/${param.id}`
+    );
+    setProduct(result.data.data);
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
   return (
     <div>
       <h1>View Product Page</h1>
       <div className="view-product-container">
-        <h2>Product Title</h2>
-        <p>Content</p>
+        <h2>{product.name}</h2>
+        <p>{product.price} THB</p>
+        <p>{product.description}</p>
       </div>
-      <button>Back to Home</button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Back to Home
+      </button>
     </div>
   );
 }
